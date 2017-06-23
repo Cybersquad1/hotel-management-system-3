613fbf340fa771621aef8bd0f8c13a0375a0d45d
Chris Morgan <cmorgan@inspirato.com>
2017-06-22 21:41:44 -0600

    CP Adds shell for ITOps service

---
+++ src/ITOps/Config/EndpointConfig.cs
@@ -0,0 +1,41 @@
+using System;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace ITOps.Config
+{
+	[EndpointName("HMS.ITOps")]
+	public class EndpointConfig : IConfigureThisEndpoint, AsA_Server
+	{
+		private static readonly ILog Log = LogManager.GetLogger<EndpointConfig>();
+
+		public EndpointConfig()
+		{
+			LogManager.Use<DefaultFactory>();
+
+			if (Environment.UserInteractive)
+				Console.Title = "HMS.ITOps";
+		}
+
+		public void Customize(EndpointConfiguration endpointConfiguration)
+		{
+			Log.Info("Customize...");
+
+			var container = ContainerSetup.Create();
+
+			endpointConfiguration.UseSerialization<JsonSerializer>();
+			endpointConfiguration.Recoverability().Delayed(c => c.NumberOfRetries(0));
+			endpointConfiguration.UseContainer<AutofacBuilder>(c => c.ExistingLifetimeScope(container));
+			endpointConfiguration.UsePersistence<InMemoryPersistence>();
+			endpointConfiguration.SendFailedMessagesTo("error");
+			endpointConfiguration.AuditProcessedMessagesTo("audit");
+			endpointConfiguration.EnableInstallers();
+
+			var transport = endpointConfiguration.UseTransport<MsmqTransport>()
+				.ConnectionString("deadLetter=false;journal=false");
+
+			var routing = transport.Routing();
+			//routing.RegisterPublisher(typeof(SomeEvent), "HMS.ServiceName");
+		}
+	}
+}
